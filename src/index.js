bb3e9eaff6f78093cc5c92e4f8a32f4bdc400f08
robsonfx <robsonfx@gmail.com>
2022-11-15 19:45:43 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -9,20 +9,83 @@ app.use(cors());
 
 const users = [];
 
+
+//OK
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  //Verficando se um user existe
+  const usernameAlreadyExists = users.some((user) => user.username === username);
+
+  if (!usernameAlreadyExists) {
+    return response.status(404).json({ error: 'Username already exists' });
+  }
+  // se existe, encontra para o user na request.
+  request.user = users.find(user => user.username === username);
+ 
+  return next();
 }
 
+//ok
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length > 9)  {
+    return response.status(403).json({error: "Free tier limit exceeded!. Get PRO!"});
+  }
+    return next();
+ 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  
+  //validando se o id é uuid
+  const regexExp = /^[a-z,0-9,-]{36,36}$/;
+  
+  
+  if(!regexExp.test(id)) {
+    return response.status(400).json({error: "Invalid resource"});
+  }
+
+  if (!username) {
+    return response.status(400).json({error: "Resource not found!"});
+  }
+  
+  const user = users.find(user => user.username === username);
+
+ 
+ 
+  if(!user) {
+    return response.status(404).json({error: "Resource not found!"});
+   
+  }
+  const todoAlreadyExists = user.todos.some((td) => td.id === id);
+  if (!todoAlreadyExists) {
+    return response.status(404).json({error: "Resource not found!"});
+  }
+
+  if (user &&  todoAlreadyExists) {
+    request.todo = user.todos.find(td => td.id === id);
+    request.user = user;
+  }
+  return next();
+  
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  //Verficando se um user existe
+  const userAlreadyExists = users.some((user) => user.id === id);
+
+  if (!userAlreadyExists) {
+    return response.status(404).json({ error: 'Username already exists' });
+  }
+  // se existe, encontra para o user na request.
+  request.user = users.find(user => user.id === id);
+ 
+  return next();
 }
 
 app.post('/users', (request, response) => {
